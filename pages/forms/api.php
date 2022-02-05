bc981abfea7f622487497fedf64c48b37f87fd61
whynaidu <naiduvedant@gmail.com>
2022-02-05 22:28:33 +0530

    database connectivity

--- pages/forms/api.php
+++ pages/forms/api.php
@@ -1,8 +1,9 @@
 <?php
 include_once 'include.php';
-if(isset($_POST['save']))
-{	 
-	 $book_no = $_POST['book_no'];
+if(isset($_POST['submit']))
+{  
+
+    $book_no = $_POST['book_no'];
 	 $date = $_POST['date'];
 	 $complaint_no = $_POST['complaint_no'];
 	 $district = $_POST['district'];
@@ -26,19 +27,19 @@ if(isset($_POST['save']))
 	 $medical_conformatio_31_report = $_POST['medical_conformatio_31_report'];
 	 $discription = $_POST['discription'];
 
+$sql = "INSERT INTO complaint_form (book_no,date,complaint_no,district,taluka,
+police_station,sub_police_station,section,type_of_offence,complaint_filer_name,
+complaint_filer_address,victim_name,criminal_name,criminal_address,category_and_cast,
+cast_certificate,fir,aadhaar_card,bank_info,charge_sheet,medical_report,
+medical_conformatio_31_report,discription)
+VALUES ('$book_no','$date','$complaint_no','$district', '$taluka', '$police_station','$sub_police_station', '$section', '$type_of_offence','$complaint_filer_name', '$complaint_filer_address', 
+'$victim_name','$criminal_name', '$criminal_address', '$category_and_caste','$caste_certificate', '$fir', '$aadhaar_card','$bank_info', '$charge_sheet', '$medical_report','$medical_conformatio_31_report','$discription')";
 
-
-	 $sql = "INSERT INTO complaints (book_no,date,complaint_no,district,taluka,
-	 police_station,sub_police_station,section,type_of_offence,complaint_filer_name,
-	 complaint_filer_address,victim_name,criminal_name,
-	 criminal_address,category_and_caste,caste_certificate,fir,aadhaar_card,bank_info,charge_sheet,
-	 medical_report,medical_conformatio_31_report,discription)
-	 VALUES ('$complaint_no')";
-	 if (mysqli_query($conn, $sql)) {
-		echo "New record created successfully !";
-	 } else {
-		echo "Error: " . $sql . " " . mysqli_error($conn);
-	 }
-	 mysqli_close($conn);
+if (mysqli_query($conn, $sql)) {
+	header('Location: governmentform.php');
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-?>
+		mysqli_close($conn);
+}
+?> 
